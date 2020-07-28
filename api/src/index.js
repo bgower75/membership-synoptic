ba7944d429d98cc5d3d86062d1c421f3f730bde2
Belinda Gower <25243519+bgower75@users.noreply.github.com>
2020-07-28 15:59:48 +0100

    Revert "Feature/schema"

--- api/src/index.js
+++ api/src/index.js
@@ -7,14 +7,6 @@ const app = express()
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(function(req, res, next){
-    res.setTimeout(120000, function(){
-        console.log('Request has timed out.');
-            res.send(408);
-        });
-
-    next();
-});
 app.use ('/api', require('./router/membership-router'));
 
 module.exports = app
