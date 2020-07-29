adc5db0fa9ac14724944f877d135fbd3ac24b461
Belinda Gower <belinda.gower@simplyhealth.co.uk>
2020-07-29 08:52:10 +0100

    corrected connection route

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config({silent: true}); //this allows the app to continue running if it can't find the env file;
 const mongoose = require('mongoose');
-const server = require('../src');
+const server = require('./api');
 
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(`mongodb://localhost:27019/memberships`, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('connected to database');
         server.listen(process.env.PORT, () => {
             console.log(`Server is running on port ${process.env.PORT}`);
         });
-    });
+    })
     .catch((error) => {
         console.error(`the error is ...${error}`);
 });
