5262b16f3dcad6b73dfc550ebc6dc13c9fffdda9
Belinda Gower <belinda.gower@simplyhealth.co.uk>
2020-07-30 09:23:18 +0100

    added logging with timestamp

--- src/api/controller/membership-functionality.js
+++ src/api/controller/membership-functionality.js
@@ -3,22 +3,26 @@ const mongoose = require('mongoose');
 const Member = mongoose.model("Member", membershipSchema);
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
-const { number } = require('yargs');
-const { isNumber } = require('util');
+// const { number } = require('yargs');
+// const { isNumber } = require('util');
+const logger = require('../../utilities/logger');
 
 async function getAllMemberships(req, res) {
     try {
         let returnedMembers = []
         returnedMembers = await Member.find();
         if(returnedMembers.length === 0) {
+            logger.error('No members found');
             res.status(404).json({
                 "message": "No members found"
             });
         } else {
+            logger.info(`Members found`);
             res.status(200).send(returnedMembers)
         };
     }
     catch(err) {
+        logger.error(`Unable to complete request ${err}`);
         res.status(500).json({
             "message": `Unable to complete request. ${err}`
         });
@@ -28,11 +32,15 @@ async function getMemberById(req, res) {
     let returnedMember
     try {
         returnedMember = await Member.findById(req.params.id)
+        logger.info(`Member with id ${req.params.id} found`)
         returnedPin = comparePin(req.body.pin, returnedMember.pin)
         if(returnedPin) {
+            logger.info(`Member authentication successful`);
             let message = `Hello ${returnedMember.name}`
-            res.status(200).send(message + ' ' + returnedMember);
+            res.write( `${message} ${returnedMember}`)
+            res.end()
         } else {
+            logger.error(`incorrect pin ${req.params.pin} ${err}`);
             res.status(400).json({
                 "message": `Pin incorrect`
             })
@@ -40,10 +48,12 @@ async function getMemberById(req, res) {
     }
     catch(err) {
         if(!returnedMember) {
+            logger.error(`No card found with id ${req.params.id} ${err}`);
             res.status(404).json({
                 "message": `Card not found. Please register your card. ${err}`
             });
         } else {
+            logger.error(`Unable to complete request ${err}`);
             res.status(500).json({
                 "message": `Unable to complete request. ${err}`
             });
@@ -64,11 +74,12 @@ async function createNewMember(req, res) {
         newMember.pin = hash;
         newMember.validate((err) => {
             if(err) {
-                // logger.err(`Wrong format to create new vinyl ${err}`);
+                logger.error(`Wrong format to create new member ${err}`);
                 res.status(400).json({
                     "message": `Wrong format to create a new member ${err}`
                 });
             }else {
+                logger.info(`Member ${newMember.id} created`)
                 newMember.save();
                 let message = `Hello ${newMember.name}`
                 res.status(201).send(message + ' ' + newMember);
@@ -76,6 +87,7 @@ async function createNewMember(req, res) {
         })
     }
     catch(err) {
+        logger.error(`Unable to complete request ${err}`);
         res.status(500).json({
             "message": `Unable to complete request. ${err}`
         })
@@ -93,19 +105,19 @@ async function updateMember(req, res) {
    try{
        foundMember.validate((err)  => {
            if(err) {
-               console.log(err)
-            // logger.error(`Wrong format to update a member`);
+               logger.error(`Wrong format to update a member`);
                res.status(400).json({
                   "message": `wrong format to update a member`
                });
            }else{
+               logger.info(`${foundMember.id} balance updated successfully`)
                foundMember.save()
                res.status(200).send(foundMember)
            }
        })
    }
    catch (err) {
-       // logger.error(`No member with employee id ${req.params.empid} found`);
+       logger.error(`Card not registered ${err}`);
        res.status(404).json({
            "message": `Card not registered. Please register your card.`
        });
@@ -114,16 +126,19 @@ async function updateMember(req, res) {
 async function deleteMember(req, res) {
     try{
         await Member.findByIdAndDelete(req.params.id);
+        logger.info(`Member ${req.params.id} found and deleted`);
         res.status(200).json({
             "message": `Member ${req.params.id} has been sucessfully deleted.`
         });
     }
     catch(err) {
         if(err.name == 'CastError') {
+            logger.error(`Member with id ${req.params.id} not found`);
             res.status(404).json({
                 "message": `No member found. ${err}`
             })
         } else {
+            logger.error(`Unable to complete request ${err}`);
             res.status(500).json({
                 "message": `Unable to complete request. ${err}`
             })
@@ -144,10 +159,12 @@ async function authenticate(req, res) {
     }
     catch(err) {
         if(!returnedMember) {
+            logger.error(`Card not found ${err}`);
             res.status(404).json({
                 "message": `Card not found. Please register your card. ${err}`
             });
         } else {
+            logger.error(`Unable to complete request ${err}`);
             res.status(500).json({
                 "message": `Unable to complete request. ${err}`
             });
@@ -158,8 +175,10 @@ async function authenticate(req, res) {
 async function comparePin(userPin, hash) {
     bcrypt.compareSync(userPin, hash, function (err){
         if(err) {
+            logger.error(`Unable to complete request ${err}`);
             return false
         } else {
+            logger.info(`Password matches`);
             return true
         }
     })
