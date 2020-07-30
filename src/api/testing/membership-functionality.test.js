154231b6bfe07916614b98648464dcfd506167b7
Belinda Gower <belinda.gower@simplyhealth.co.uk>
2020-07-30 16:07:05 +0100

    updated unit tests

--- src/api/testing/membership-functionality.test.js
+++ src/api/testing/membership-functionality.test.js
@@ -1,12 +1,12 @@
 const mockingoose = require('mockingoose').default
 const Member = require('../models/membership-model')
-const {createNewMember, getAllMemberships, getMemberById, deleteMember, updateMember}  = require('../controller/membership-functionality')
+const {createNewMember, getAllMemberships, getMemberByCardId, deleteMember, updateMember}  = require('../controller/membership-functionality')
 
 describe("members", () => {
     it('should be defined', () => {
         expect(createNewMember).toBeDefined()
         expect(getAllMemberships).toBeDefined()
-        expect(getMemberById).toBeDefined()
+        expect(getMemberByCardId).toBeDefined()
         expect(deleteMember).toBeDefined()
         expect(updateMember).toBeDefined()
     })
@@ -17,7 +17,8 @@ describe("members", () => {
             empid: 11, 
             email: 'belinda.gower@test.com',
             mobile: "01234567890",
-            pin: '1234'
+            pin: '1234',
+            balance: 0
         }
         mockingoose(Member).toReturn(mockMember, 'save')
         const response = await Member.create(mockMember)
@@ -26,45 +27,49 @@ describe("members", () => {
         expect(response.email).toBe(mockMember.email)
         expect(response.mobile).toBe(mockMember.mobile)
         expect(response.pin).toBe(mockMember.pin)
+        expect(response.balance).toBe(mockMember.balance)
     })
 
     it('should return a member', async () => {
         const mockResponse = {
             _id: '5f0c80c447641625d017c649',
             name: 'Belinda Gower Test',
-            balance: '10.00',
+            balance: '10',
             empid: 1, 
             email: 'belinda.gower@test.com',
             mobile: '01234567890',
-            pin: '1234'
+            pin: '$2b$10$aB5iGOX5wq97xEphUlhqWeUjx2I5ETpfITLfA6l02hWretXhOPQDi',
+            cardId: 'E7sKQ5tKNyxIfxZ4'
         }
         mockingoose(Member).toReturn(mockResponse, 'findOne')
-        const response = await Member.findById(mockResponse)
+        const response = await Member.findOne(mockResponse)
         expect(JSON.parse(JSON.stringify(response._id))).toBe(mockResponse._id)
     })
     it('should delete a member', async () => {
         const mockResponse = {
             _id: '5f0c80c447641625d017c649',
             name: 'Belinda Gower Test',
-            balance: '10.00',
+            balance: '10',
             empid: 1, 
             email: 'belinda.gower@test.com',
             mobile: '01234567890',
-            pin: '$2b$10$vfUSEhcov8w18P4Ui/Slh.4P8RQUYhewyUBwXSC8fxuwlUk1EhUke'
+            pin: '$2b$10$vfUSEhcov8w18P4Ui/Slh.4P8RQUYhewyUBwXSC8fxuwlUk1EhUke',
+            cardId: 'E7sKQ5tKNyxIfxZ4'
         }
         mockingoose(Member).toReturn(mockResponse, 'findOne')
-        const response = await Member.findByIdAndDelete(mockResponse._id)
+        const response = await Member.findOneAndDelete(mockResponse._id)
         expect(response).toBe(undefined)
     })
     it('should update a member', async() => {
         const mockResponse =  {
             _id: '5f0c80c447641625d017c649',
             name: 'Belinda Gower Test',
-            balance: '10.00',
+            balance: '10',
             empid: 1, 
             email: 'belinda.gower@test.com',
             mobile: '01234567890',
-            pin: '$2b$10$vfUSEhcov8w18P4Ui/Slh.4P8RQUYhewyUBwXSC8fxuwlUk1EhUke'
+            pin: '$2b$10$vfUSEhcov8w18P4Ui/Slh.4P8RQUYhewyUBwXSC8fxuwlUk1EhUke',
+            cardId: 'E7sKQ5tKNyxIfxZ4'
         }
         mockingoose(Member).toReturn(mockResponse, 'findOne')
         const change = 'changedName'
